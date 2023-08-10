ce1132f8cacd1324f2bb48a8a4538f949eeccc8f
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-10 12:17:26 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,17 @@
-    #!/usr/bin/python3
-
-    if __name__ == "__main__":
-        """Print the number of and list of arguments."""
-        import sys
-        av = len(sys.argv) - 1
-
-        if av == 0:
-            print("{} arguments.".format(av))
-        elif av == 1:
-            print("{} argument:".format(av))
-        else:
-            print("{} arguments:".format(av))
-
-        if av >= 1:
-            av = 0
-            for i in sys.argv:
-                if av != 0:
-                    print("{}: {}".format(av, i))
-                av += 1
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+    av = len(sys.argv) - 1
+    if av == 0:
+        print("{} arguments.".format(av))
+    elif av == 1:
+        print("{} argument:".format(av))
+    else:
+        print("{} arguments:".format(av))
+    if av >= 1:
+        av = 0
+        for i in sys.argv:
+            if av != 0:
+                print("{}: {}".format(av, i))
+            av += 1
