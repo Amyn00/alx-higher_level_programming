ddfdc7fe38e98a6a7c02c67995f3e4ce5e10e91a
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-28 07:07:09 -0700

    task 101 done

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,5 @@ def safe_function(fct, *args):
         res = fct(*args)
         return (res)
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
