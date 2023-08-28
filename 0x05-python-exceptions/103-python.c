e3f49bac14d2c6949a9ae783c6e1c8182568b106
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-28 07:34:28 -0700

    notdone

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+/**
+ * print_python_list - print python list
+ * @p: input
+ */
+void print_python_list(PyObject *p){}
+/**
+ * print_python_bytes - print python bytes
+ * @p: input
+ */
+void print_python_bytes(PyObject *p){}
+/**
+ * print_python_float - print python float
+ * @p: input
+ */
+void print_python_float(PyObject *p){}
