f2b5ce4785dfa851837bf680dd232320a316d7bd
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-29 06:28:07 -0700

    task 3 done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""write a class Squre that defines a square by:(based on 2-square.py)"""
+
+
+class Square:
+    """class - square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise (TypeError("size must be integer"))
+        elif size < 0:
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
