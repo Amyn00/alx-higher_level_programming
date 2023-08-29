79de221ad9f74c48e17fcea7e55178d8013f8aaa
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-08-29 06:05:03 -0700

    task 1 done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Class Square with size"""
 
+
 class Square:
     """Class to create a square"""
     def __init__(self, square_size):
+        """Constructor of the Square"""
         self.__size = square_size
