6f5e352c29729eb4c9ee240a109d904a6972eb5c
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-09-11 06:21:57 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script task 1. My list.
+"""
+
+
+class Mylist(list):
+    """
+    Write a class MyList that inherits from list:
+
+    """
+    def print_sorted(self):
+        print(sorted(self))
