c48965454647fa4a7c0673c8c06fb850f0f843fd
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-09-11 05:24:29 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+script task 0
+"""
+
+
+def lookup(obj):
+    """
+    a function that returns the list of available attributes and method
+    of an object:
+
+    """
+
+    return dir(obj)
