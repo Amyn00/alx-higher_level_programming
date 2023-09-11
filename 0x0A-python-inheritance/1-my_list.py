d8cceec9304564275aaa452901434598403a6db5
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-09-11 06:57:43 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,8 +3,25 @@
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """
+    A custom list class that inherits from the built-in list class.
 
+    Public Methods:
+    - print_sorted(self): Print the list in ascending order.
+
+    Usage:
+        my_list = MyList()
+        my_list.append(1)
+        my_list.append(4)
+        my_list.append(2)
+        my_list.append(3)
+        my_list.append(5)
+        print(my_list)      # Output: [1, 4, 2, 3, 5]
+        my_list.print_sorted()  # Output: [1, 2, 3, 4, 5]
+        print(my_list)      # Output: [1, 4, 2, 3, 5]
+                                                                                
+    """
+    
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
+        """Print the list in sorted ascending order."""
         print(sorted(self))
