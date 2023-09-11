cf609c54f64908a0fcc3f197bb7500681e617426
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-09-11 07:39:32 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a class based 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
