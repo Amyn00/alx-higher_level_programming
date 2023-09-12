8e9e32b4b12c46a9e2ecca6cc85f5c93a0d6818e
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-09-12 06:43:26 -0700

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8).
+    Args:
+        filename (str): input
+        text (str): input
+    Returns:
+        The number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
