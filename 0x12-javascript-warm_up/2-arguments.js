e725b0326084b245e3e527222faab21093fa6826
Amyn00 <medamine190@gmail.com>
2023-11-13 07:37:23 -0800

    tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length === 2)
-{
+if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.lenght === 3) {
   console.log('Argument found');
