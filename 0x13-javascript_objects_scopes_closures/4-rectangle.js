044f7f5efa4c8c1fe0ba3dc8f77805cc2c9f3290
Amyn00 <medamine190@gmail.com>
2023-11-14 05:10:33 -0800

    tasks

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -23,9 +23,9 @@ class Rectangle {
   }
 
   rotate () {
-    const tmp = this.width;
+    const aux = this.width;
     this.width = this.height;
-    this.height = tmp;
+    this.height = aux;
   }	
 }
 
