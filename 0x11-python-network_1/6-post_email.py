d259e4dbc8815366011cc6c94877531730291bfa
Amyn00 <medamine190@gmail.com>
2023-12-01 02:58:37 -0800

    update

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    request = requests.post(url, data=value)
+    print(request.text)
