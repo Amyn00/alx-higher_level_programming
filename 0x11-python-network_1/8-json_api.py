e2da7cea0f9c3e9bd1ccf352f64fac61b6985bca
Amyn00 <medamine190@gmail.com>
2023-12-01 06:00:16 -0800

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script tha:
+"""A script that:
 - takes in a letter
 - sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
@@ -11,9 +11,10 @@ import requests
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-    request = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = request.json()
+        response = r.json()
         if response == {}:
             print("No result")
         else:
