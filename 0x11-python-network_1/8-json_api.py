d538c589a6d34d70a811bceea3b20bebc37a0537
amyn00 <medamine190@gmail.com>
2024-01-10 11:57:22 -0800

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
+"""script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 import sys
 import requests
